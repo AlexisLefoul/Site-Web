edd1a6b6743bbd2d538c5afc1fd4b1f2bc603658
AlexisLefoul <alex.lefoul@orange.fr>
2020-07-26 15:18:40 +0200

    ddd

--- _site/js/script.js
+++ _site/js/script.js
@@ -21,3 +21,25 @@ window.onclick = function(event) {
   }
 };
 
+var modal2 = document.getElementById("myModal2");
+
+var btn2 = document.getElementById("myBtn2");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn2.onclick = function() {
+  modal2.style.display="block";
+  modal2.style.transform = "scale(1)";
+  modal2.style.transition = "0.5s";
+};
+
+span.onclick = function() {
+  modal2.style.transition = "0.5s";
+  modal2.style.transform = "scale(0)";
+};
+
+window.onclick = function(event) {
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+};
